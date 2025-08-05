fe6dd46d9f7ee0376f21bd1a613d5c19ce9a2320
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-05 10:30:09 -0300

    Delete exercicios directory

--- exercicios/EstruturaDeDados/sc.js
+++
@@ -1,4 +0,0 @@
-const ListaDeCompra = ["pasta","pipoca","prato","doce","fruta"];
-ListaDeCompra.push("tv")
-ListaDeCompra.shift();
-console.log(ListaDeCompra.length);
--- exercicios/LaçosDeRepetição/ex003/script.js
+++
@@ -1,9 +0,0 @@
-function clique(){
-const numF = document.querySelector("input").value;
-const h2 = document.querySelector("h2");
-const res = document.querySelector("#res");
-    h2.innerHTML = `Contando de 0 até ${numF}`;
-for(let i = 1;i <= numF;i++){
-    res.textContent += `${i} =>` 
-    }
-}
