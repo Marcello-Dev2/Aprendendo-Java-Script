9633f8ab02453e172eafa2e20ef32cacc0994a94
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-07 11:00:07 -0300

    tabuada 100%
    Começando outro projeto Lista de tarefa

--- Projetos/Calculadora/script.js
+++ Projetos/Calculadora/script.js
@@ -3,3 +3,15 @@ function adicionarCaracter(num){
 
     document.querySelector(".display").value = display + num
 }
+function limpaTela(){
+    document.querySelector(".display").value = ""
+}
+function calcular(){
+    const display = document.querySelector(".display").value;
+    document.querySelector(".display").value = eval(display)
+}
+function inverterNumero(){
+    const display = document.querySelector(".display").value;
+
+    document.querySelector(".display").value = display * - 1 
+}
