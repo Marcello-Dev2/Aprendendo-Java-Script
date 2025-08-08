b77b9a6f5838e511715c542bc1b4bd97a597cf94
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-08 11:32:50 -0300

    btn iniciar funcionando
    agora tenho que dar função aos outros btn

---
+++ Projetos/cronometro/script.js
@@ -0,0 +1,45 @@
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milli");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer)
+
+function startTimer() {
+    interval = setInterval(() => {
+
+        if (!isPaused) {
+            milliseconds += 10
+
+            if (milliseconds === 1000) {
+                seconds++;
+                milliseconds = 0
+
+            } if (seconds === 60) {
+                minutes++
+                seconds = 0;
+            }
+
+            minutesEl.textContent = formatTimer(minutes);
+            secondsEl.textContent = formatTimer(seconds);
+            millisecondsEl.textContent =formatMilli(milliseconds);
+        }
+    }, 10)
+}
+function formatTimer(time){
+    return time < 10 ? `0${time} `: time
+}
+function formatMilli(time){
+    return time < 100 ? `${time}`.padStart(3, "000") : time ;
+}
+resetBtn.addEventListener("click", reset)
+
