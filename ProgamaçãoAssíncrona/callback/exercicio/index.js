ae6a40722d6b97410f0766df88eba1b90a455223
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-15 11:02:03 -0300

    Práticando a callback

--- ProgamaçãoAssíncrona/callback/exercicio/index.js
+++ ProgamaçãoAssíncrona/callback/exercicio/index.js
@@ -2,8 +2,9 @@ const buscarUssuario = (id, callback) => {
     setTimeout(() => {
         console.log("Bem vindo")
        const user = ({id : id, nome :"maria"})
+        const user = ({ id: id, nome: "maria" })
         callback(user)
-    },2000)
+    }, 2000)
 }
 
 buscarUssuario(1, (usuario) => {
---
+++ ProgamaçãoAssíncrona/callback/exercicio/main.js
@@ -0,0 +1,15 @@
+const buscarUsuario = (id, callback) => {
+    
+    setTimeout(() => {
+        console.log("Olá, seja Bem Vindo");
+        const user = {id : id, nome : "Marcello",
+            
+        };
+        callback(user)
+    }, 2000);
+};
+
+
+buscarUsuario(1, (usuario) =>  {
+    console.log("Usuário Encotrado", usuario)
+});
