2eee5c5f0a0aeb547fa6ee3eca780b00bb942135
MarcelloDev <marcelodevgithub@gmail.com>
2025-09-04 11:18:22 -0300

    Create texto_bind.js

---
+++ This_Bind/bind/texto_bind.js
@@ -0,0 +1,4 @@
+//Você precisa passar uma função como callback
+//Você quer garantir que 'this' sempre aponte para um objeto específico
+//Você está trabalhando com event listeners em classes
+//Você usa setTimeout/setInterval com métodos de objeto
