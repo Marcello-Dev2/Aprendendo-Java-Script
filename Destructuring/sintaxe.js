fa1cdf7f6eeb1f13abb1faa15b49a0d1c79824b8
MarcelloDev <marcelodevgithub@gmail.com>
2025-09-10 10:44:07 -0300

    1

---
+++ Destructuring/sintaxe.js
@@ -0,0 +1,20 @@
+/*const user= { nome: "marcello" , age: 12, tel: 3043740289 } 
+const {nome, age, tel } = user 
+
+console.log(user)*/
+
+/*
+function getDados(){
+    return { nome: "marcello" , age: 9 } 
+};
+
+//const { nome, age } = getDados();
+console.log(getDados())
+*/
+
+const colors  = ["red", "blue", "black"];
+const [Vermelho, azul, preto] = colors;
+
+console.log(Vermelho)
+console.log(azul)
+console.log(preto)
